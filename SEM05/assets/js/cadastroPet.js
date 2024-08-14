e31f2599501b9360d02b838a915188fbb7bd9625
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-08-13 21:27:50 -0300

    feat: codigo cadastroPet

--- SEM05/assets/js/cadastroPet.js
+++ SEM05/assets/js/cadastroPet.js
@@ -1,17 +1,21 @@
 const form = document.querySelector('form')
 
+form.addEventListener("submit", adicionarPet)
+
 function adicionarPet(event) {
     event.preventDefault()
 
-    const inputNome = document.getElementById('nome')
+    const inputNome = document.getElementById('nome').value
 
-    const inputIdade = document.getElementById('idade')
+    const inputIdade = document.getElementById('idade').value
 
-    const inputColor = document.getElementById('color')
+    const inputColor = document.getElementById('color').value
 
-    const textareaDescricao = document.getElementById('descricao')
+    const textareaDescricao = document.getElementById('descricao').value
+
+    const selectEspecie = document.getElementById('especie').value
+
 
     console.log("pet adicionado")
 }
 
-form.addEventListener("submit", adicionarPet)
